c43aaa30aacf2c594436edd26b998854c15a6cc2
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2020-07-26 22:41:23 +0200

    Oops: logic issue

--- src/utils.ts
+++ src/utils.ts
@@ -9,4 +9,4 @@ export function uuidv4() {
 
 export const randomListLength = () => Math.round(Math.random() * 10);
 
-export const takeRandom = <T>(arr: T[]) => arr[Math.round(Math.random() * arr.length)];
+export const takeRandom = <T>(arr: T[]) => arr[Math.floor(Math.random() * arr.length)];
